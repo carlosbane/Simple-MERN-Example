b26633d99de951668220ff24873e2b5fd006af66
Spiff Jekey-Green <carlosbane48@gmail.com>
2020-04-12 01:55:36 -0700

    Added a redirecting functionality

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -13,24 +13,27 @@ class Form extends React.Component {
 
     render() {
         return (
-            <form className="form card p-4 bg-dark">
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                fetch('/api/links', {
+                        method: "POST",
+                        body: JSON.stringify({ longlink: this.state.inputLink }),
+                        headers: {
+                            'Content-type': "application/json"
+                        }
+                    })
+                    .then( response => response.json())
+                    .then(data => {
+                        const link = window.location;
+                        window.location = link;
+                    });
+                }} className="form card p-4 bg-dark">
                 <input className="form-control input" onChange={(e) => {
                     this.setState({
                         inputLink: e.target.value
                     })
                 }} placeholder="Enter Url..." />
-                <input type="submit" onClick={(e) => {
-                    e.preventDefault();
-                    fetch('/api/links', {
-                            method: "POST",
-                            body: JSON.stringify({ longlink: this.state.inputLink }),
-                            headers: {
-                                'Content-type': "application/json"
-                            }
-                        })
-                        .then( response => response.json())
-                        .then(data => console.log(data));
-                    }} className="form-control btn btn-primary my-2" value="Get short Link" />
+                <input type="submit" className="form-control btn btn-primary my-2" value="Get short Link" />
             </form>
         )
     }
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class Table extends React.Component {
     constructor() {
@@ -22,43 +23,53 @@ class Table extends React.Component {
 
     render() {
         return(
-            <div>
-                <table className="table table-striped table-responsive my-2">
-                    <thead>
-                        <tr>
-                            <th>S/No.</th>
-                            <th>Full URL</th>
-                            <th>Short URL</th>
-                            <th>Options</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>*</td>
-                            <td><a href="https://example.com">https://example.com</a></td>
-                            <td><a href="/123456">123456</a></td>
-                            <td><button className="btn btn-danger" disabled>Delete</button></td>
-                        </tr>
-                        {this.state.data.map((link, index) => {
-                            return(
-                                <tr key={index+2}>
-                                    <td key={index+3}>{index+1}.</td>
-                                    <td key={index+4}><a href={link.longlink}>{link.longlink}</a></td>
-                                    <td key={index+5}><a href={link.longlink}>{link.shortlink}</a></td>
-                                    <td key={index+6}><button className="btn btn-danger" onClick={() => {
-                                        console.log(link._id);
-                                        fetch(`api/links/${link._id}`, {
-                                            method: "DELETE",
-                                        })
-                                        .then( response => response.json())
-                                        .then(data => console.log(data));
-                                    }}>Delete</button></td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </div>
+            <Router>
+                <div>
+                    <table className="table table-striped table-responsive my-2">
+                        <thead>
+                            <tr>
+                                <th>S/No.</th>
+                                <th>Full URL</th>
+                                <th>Short URL</th>
+                                <th>Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>*</td>
+                                <td><a href="https://example.com">https://example.com</a></td>
+                                <td><a href="/123456">123456</a></td>
+                                <td><button className="btn btn-danger" disabled>Delete</button></td>
+                            </tr>
+                            {this.state.data.map((link, index) => {
+                                return(
+                                        <tr key={index+2}>
+                                            <td key={index+3}>{index+1}.</td>
+                                            <td key={index+4}><a href={link.longlink}>{link.longlink}</a></td>
+                                            <td key={index+5}><a href={link.longlink}>
+                                                <Switch>
+                                                    <Route path={link.shortlink} component={() => window.location = (link.longlink)} />
+                                                </Switch>
+                                                {link.shortlink}
+                                            </a></td>
+                                            <td key={index+6}><button className="btn btn-danger" onClick={() => {
+                                                console.log(link._id);
+                                                fetch(`api/links/${link._id}`, {
+                                                    method: "DELETE",
+                                                })
+                                                .then( response => response.json())
+                                                .then(data => {
+                                                    const link = window.location;
+                                                    window.location = link;
+                                                });
+                                            }}>Delete</button></td>
+                                        </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </Router>
         )
     }
 }
